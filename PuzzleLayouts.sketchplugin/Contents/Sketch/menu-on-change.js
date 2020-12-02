c07ffa5039a0513027aaba458e37b349b4e9762f
Maxim Bazarov <maxim.bazarov@ingrammicro.com>
2020-12-02 20:58:13 +0300

    Updated

--- PuzzleLayouts.sketchplugin/Contents/Sketch/menu-on-change.js
+++ PuzzleLayouts.sketchplugin/Contents/Sketch/menu-on-change.js
@@ -1,3 +1,14 @@
+var ResizingConstraint = {
+    NONE: 0,
+    RIGHT: 1 << 0,
+    WIDTH: 1 << 1,
+    LEFT: 1 << 2,
+    BOTTOM: 1 << 3,
+    HEIGHT: 1 << 4,
+    TOP: 1 << 5
+}
+
+
 const sketch = require('sketch')
 
 var onDocumentChanged = function (context) {
@@ -8,11 +19,19 @@ var onDocumentChanged = function (context) {
         var change = changes[i];
         var path = change.fullPath();
         var type = change.type();
+        let parent = null
 
         switch (type) {
-            case 1: // Property change
+            case 1: // Property chang
                 log(`Property changed at ${path}`);
-                handleGroupChanges(sketch.fromNative(change.object()).parent)
+                //                
+                const i = path.indexOf("].overrideValues")
+                if (i >= 0)
+                    parent = getChangeParent(document, change)
+                else
+                    parent = sketch.fromNative(change.object()).parent
+                //
+                handleGroupChanges(parent)
                 break;
             case 2: // Deletion
                 // Objects that got moved in the tree are both deleted from the tree
@@ -20,7 +39,7 @@ var onDocumentChanged = function (context) {
                 if (change.isMove()) break;
 
                 log(`Object deleted at ${path}`);
-                const parent = getChangeParent(document, change)
+                parent = getChangeParent(document, change)
                 log("parent: " + parent.name)
                 handleGroupChanges(parent)
                 break;
@@ -45,7 +64,14 @@ var onDocumentChanged = function (context) {
 
 
 function getChangeParent(document, change) {
-    return eval(`document.${change.fullPath().toString().match(/(.*)\./)[1]}`)
+    let path = change.fullPath().toString()
+    const i = path.indexOf("].overrideValues")
+    log(i)
+    if (i >= 0) {
+        path = path.substring(0, i + 1)
+    }
+    log(path)
+    return eval(`document.${path.match(/(.*)\./)[1]}`)
 }
 
 function handleGroupChanges(parent) {
@@ -56,6 +82,7 @@ function handleGroupChanges(parent) {
 }
 
 function adjustLayers(layers, isX) {
+    log("ADJUST LAYERS")
     if (isX)
         layers = layers.slice().sort((a, b) => a.frame.x + a.frame.width - (b.frame.x + b.frame.width))
     else
@@ -65,6 +92,7 @@ function adjustLayers(layers, isX) {
     const perpSpacerName = "@" + (isX ? "Y" : "X") + "Spacer@"
 
     layers.forEach(function (child, index) {
+        log("child name: " + child.name)
         const isSpacer = child.name.includes(spacerName)
             || ("SymbolInstance" == child.type && child.master && child.master.layers[0] && child.master.layers[0].name.includes(spacerName))
 
@@ -74,17 +102,31 @@ function adjustLayers(layers, isX) {
         let oldPos = null
 
         //log(layers.slice(index + 1))
+        let backLayers = []
         layers.slice(index + 1).forEach(function (l) {
             const isPerpSpacer = l.name.includes(perpSpacerName)
                 || ("SymbolInstance" == l.type && l.master && l.master.layers[0] && l.master.layers[0].name.includes(perpSpacerName))
             if (isPerpSpacer) return
+
+            // Skip cards and other full size layers
+            if (18 == l.sketchObject.resizingConstraint()) {
+                backLayers.push(l)
+                return
+            }
+            //log("name: " + l.name)
+            //log(l.sketchObject.resizingConstraint())
+            /*const c = getLayerConstrains(l)
+            if (isX) {
+                if (c.right) return
+            } else {
+                if (c.bottom) return
+            }*/
             // if the next object on the same position
             const lPos = isX ? l.frame.x : l.frame.y
             if (oldPos != null && lPos == oldPos) {
                 pos = oldPos
             }
-            //            
-            log("name: " + l.name)
+            //                        
             const delta = pos - lPos
             if (isX)
                 l.frame.x += delta
@@ -93,5 +135,27 @@ function adjustLayers(layers, isX) {
             oldPos = pos
             pos += isX ? l.frame.width : l.frame.height
         }, this)
+
+        // Check if we need to resize back layers
+        backLayers.forEach(function (l) {
+            // need to increase height of back layer
+            if ((l.frame.y + l.frame.height) < pos) {
+                l.frame.height = pos - l.frame.y
+            }
+        })
+
     }, this)
+}
+
+function getLayerConstrains(layer) {
+    const resizingConstraint = 63 ^ layer.sketchObject.resizingConstraint()
+    const res = {
+        top: (resizingConstraint & ResizingConstraint.TOP) === ResizingConstraint.TOP,
+        bottom: (resizingConstraint & ResizingConstraint.BOTTOM) === ResizingConstraint.BOTTOM,
+        left: (resizingConstraint & ResizingConstraint.LEFT) === ResizingConstraint.LEFT,
+        right: (resizingConstraint & ResizingConstraint.RIGHT) === ResizingConstraint.RIGHT,
+        height: (resizingConstraint & ResizingConstraint.HEIGHT) === ResizingConstraint.HEIGHT,
+        width: (resizingConstraint & ResizingConstraint.WIDTH) === ResizingConstraint.WIDTH
+    }
+    return res
 }
